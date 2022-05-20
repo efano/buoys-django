eeab0164b4df7720ef99c1b09383632af6409597
Lis Fano <lisfano@gmail.com>
2022-05-19 19:53:43 -0700

    added navbar login/logout buttons

--- buoys_app/urls.py
+++ buoys_app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 
     path('', views.index, name='index'),
     path('register/', views.register_request, name='register'),
-    path('login/', views.login_request, name='login')
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout')
 
 ]
--- buoys_app/views.py
+++ buoys_app/views.py
@@ -6,7 +6,7 @@ from siphon.simplewebservice.ndbc import NDBC
 import requests
 from .forms import *
 from django.contrib.auth import login
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 
@@ -16,7 +16,7 @@ df = NDBC.latest_observations()
 
 # set the spatial extent to Great Lakes region
 df = df.loc[df['latitude'].between(40.53, 49.15) & df['longitude'].between(
-    -95.30, -74.36), ['latitude', 'longitude', 'station', 'time', 'wind_direction', 'wind_speed', 'wind_gust', 'wave_height', 'pressure', '3hr_pressure_tendency', 'air_temperature', 'water_temperature', 'dewpoint', 'visibility', 'water_level_above_mean', 'dominant_wave_period', 'average_wave_period']].sort_values(by=['station'])
+    -95.30, -74.36), ['latitude', 'longitude', 'station', 'time', 'wind_direction', 'wind_speed', 'wind_gust', 'wave_height', 'pressure', '3hr_pressure_tendency', 'air_temperature', 'water_temperature', 'dewpoint', 'visibility']].sort_values(by=['station'])
 
 df = df.rename(columns={'3hr_pressure_tendency': 'pressure_tendency_3hr'})
 
@@ -35,11 +35,11 @@ def register_request(request):
             user = form.save()
             login(request, user)
             messages.success(request, 'Registration successful.')
-            return redirect('/index/')
+            return redirect('buoys_app:index')
         messages.error(
             request, 'Unsuccessful registration. Invalid information.')
     form = NewUserForm()
-    return render(request=request, template_name='buoys_app/register.html', context={'register_form': form})
+    return render(request, 'buoys_app/register.html', {'register_form': form})
 
 
 def login_request(request):
@@ -52,10 +52,16 @@ def login_request(request):
             if user is not None:
                 login(request, user)
                 messages.info(request, f'You are now logged in as {username}.')
-                return redirect('/index/')
+                return redirect('buoys_app:index')
             else:
                 messages.error(request, 'Invalid username or password.')
         else:
             messages.error(request, 'Invalid username or password.')
     form = AuthenticationForm()
-    return render(request=request, template_name='buoys_app/login.html', context={'login_form': form})
+    return render(request, 'buoys_app/login.html', {'login_form': form})
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, 'You have successfully logged out.')
+    return redirect('buoys_app:index')
