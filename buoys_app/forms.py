70453f8be34cf2709934f3a6d8ba695d1724bbaf
Lis Fano <lisfano@gmail.com>
2022-05-25 21:32:47 -0700

    continue with comments thread

--- buoys_app/forms.py
+++ buoys_app/forms.py
@@ -6,17 +6,21 @@ from django.contrib.auth.models import User
 # Create your forms here.
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    agency = forms.CharField(max_length=100, required=True)
+    #agency = forms.CharField(max_length=100, required=False)
+    first_name = forms.CharField(max_length=12, required=True)
+    last_name = forms.CharField(max_length=12, required=True)
 
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2',
-                  'email', 'agency')
+                  'email', 'first_name', 'last_name')
 
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
-        user.agency = self.cleaned_data['agency']
+        #user.agency = self.cleaned_data['agency']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
 
         if commit:
             user.save()
--- buoys_app/models.py
+++ buoys_app/models.py
@@ -5,15 +5,11 @@ from datetime import datetime, date
 
 # Create your models here.
 class Comment(models.Model):
-    user = models.ForeignKey(
+    user = models.OneToOneField(
         User, related_name='comments', on_delete=models.CASCADE)
-    user_agency = models.CharField(max_length=100, default='')
     published_date = models.DateTimeField(auto_now_add=True)
     buoy_id = models.CharField(max_length=5, default='')
     body = models.CharField(max_length=255)
 
-    # class Meta:
-    #     ordering = ['-published_date']
-
     def __str__(self):
-        return '%s - %s' % (self.user_agency, self.user)
+        return '%s - %s' % (self.user, self.body)
